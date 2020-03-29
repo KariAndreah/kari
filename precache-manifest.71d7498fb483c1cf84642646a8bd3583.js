479506d1116edaea3b03e004eac95f71c47ead89
Kari Burt <kandreahburt@gmail.com>
2020-03-29 18:47:47 -0500

    Updates

---
+++ precache-manifest.71d7498fb483c1cf84642646a8bd3583.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1eb55b86d5ec659e067632ef82ca3d",
+    "url": "/kari/index.html"
+  },
+  {
+    "revision": "2b227d4444c61183d176",
+    "url": "/kari/static/css/main.434d099a.chunk.css"
+  },
+  {
+    "revision": "0fc230c179d8369da3f4",
+    "url": "/kari/static/js/2.b2bb792d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kari/static/js/2.b2bb792d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b227d4444c61183d176",
+    "url": "/kari/static/js/main.fd8f5bab.chunk.js"
+  },
+  {
+    "revision": "5f58dbbd8ba6706f698e",
+    "url": "/kari/static/js/runtime-main.52b5bad2.js"
+  },
+  {
+    "revision": "54b776e56430e8a00334d2e30c00e454",
+    "url": "/kari/static/media/Kari.54b776e5.jpg"
+  },
+  {
+    "revision": "98a10b7edfafa2f4ad52cfba378a9390",
+    "url": "/kari/static/media/Poppins-Regular.98a10b7e.woff"
+  },
+  {
+    "revision": "f97a4d9f2e7fa15eee353a99a68bd2f9",
+    "url": "/kari/static/media/Project.f97a4d9f.jpg"
+  },
+  {
+    "revision": "1ad78cfc6eaec2e414af8d00248d8eb1",
+    "url": "/kari/static/media/mountain.1ad78cfc.jpg"
+  }
+]);

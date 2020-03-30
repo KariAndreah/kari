df4c7554273e318df999ba0edaeddb48283e9243
Kari Burt <kandreahburt@gmail.com>
2020-03-29 22:14:12 -0500

    New Header Photo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,15 +11,15 @@ class Projects extends Component {
                 <h1 className='aboutH1'>Projects</h1>
                 <div className='containerProjects'>
                     <div className='colP'>
-                        <div ClassName='Card'>
+                        <div ClassName='card'>
                             <img src={picture} className='projectpic'
                                 alt="Project"></img>
-                            <h2>Project 1</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                <h2>Project 1</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </div>
                     </div>
                     <div className='colP'>
-                        <div ClassName='Card'>
+                        <div ClassName='card'>
                             <img src={picture} className='projectpic'
                                 alt="Project 2"></img>
                             <h2>Project 2</h2>
@@ -27,7 +27,7 @@ class Projects extends Component {
                         </div>
                     </div>
                     <div className='colP'>
-                        <div ClassName='Card'>
+                        <div ClassName='card'>
                             <img src={picture} className='projectpic'
                                 alt="Project 3"></img>
                             <h2>Project 3</h2>
